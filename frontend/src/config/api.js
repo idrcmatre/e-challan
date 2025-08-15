c9b0952ba3c003b3c1ae5659432bee6ed1c2c314
Ubuntu <ubuntu@ip-172-31-127-228.ap-southeast-2.compute.internal>
2025-08-15 12:52:18 +0000

    Updated IP addresses

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,5 +1,3 @@
-const API_BASE_URL = process.env.NODE_ENV === 'production' 
-    ? 'http://3.106.138.97/api' 
-    : 'http://localhost:5001/api';
+const API_BASE_URL = 'http://3.104.65.126/api' 
 
 export default API_BASE_URL;
