d7bbbe1734de4a3c12e5822f7ca7484f50ad62a4
Vaishnav Rai <vaishnavrai9331@gmail.com>
2025-08-16 10:11:22 +1000

    Added Comments as per rubric

--- frontend/api.js
+++ frontend/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'http://3.104.65.126/api' 
+const API_BASE_URL = 'http://13.236.98.196/api' 
    
 //please work
 export default API_BASE_URL;
--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = 'http://3.104.65.126/api' 
+const API_BASE_URL = 'http://13.236.98.196/api' 
 
 export default API_BASE_URL;
